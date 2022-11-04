5c93322907d400a7bfe1cad045806deb3d2ee7a2
Naveen Busireddy <naveenbusireddy@gmail.com>
2022-11-04 09:21:08 +0530

    Added local storage and retriving the todo in initial rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  // state to keep track of the value in the input
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
 
-  const clickHandler = () => {
-    setTodos([...todos, input]);
+  // Getting local storage todos and setting into todos track 
+  const localTodos = () => {
+    const savedTodos = localStorage.getItem('todos');
+    if(savedTodos) {
+      // return the parsed JSON object back to Javascript object
+      return JSON.parse(savedTodos);
+    }
+    else {
+      return [];
+    }
+  }
+
+  // state to keep track of todos
+  const [todos, setTodos] = useState(localTodos);
+
+
+  useEffect(() => {
+    // localstorage only support storing strings as keys and values.
+    // JSON.stringify will convert the object into a JSON string.
+    
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // Function to create a new object on add button
+  const clickHandler = (event) => {
+    //prevent the browser default behavior or refreshing the page.
+    event.preventDefault();
+
+    //Don't submit if the input is an empty string
+    if (input !== "") {
+      setTodos([...todos, input]);
+    }
     setInput("");
   };
 
@@ -21,9 +51,11 @@ function App() {
       />
       <button onClick={clickHandler}>Add</button>
       <div>
-        {todos.map((todo) => (
-          <li>{todo}</li>
-        ))}
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
