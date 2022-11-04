43992016e1b56524b7d6c06b2ea0bc47b65fa44b
Naveen Busireddy <naveenbusireddy@gmail.com>
2022-11-04 16:07:44 +0530

    Delete feature added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   // state to keep track of the value in the input
   const [input, setInput] = useState("");
 
-  // Getting local storage todos and setting into todos track 
+  // Getting local storage todos and setting into todos track
   const localTodos = () => {
-    const savedTodos = localStorage.getItem('todos');
-    if(savedTodos) {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
       // return the parsed JSON object back to Javascript object
       return JSON.parse(savedTodos);
-    }
-    else {
+    } else {
       return [];
     }
-  }
+  };
 
   // state to keep track of todos
   const [todos, setTodos] = useState(localTodos);
 
-
   useEffect(() => {
     // localstorage only support storing strings as keys and values.
     // JSON.stringify will convert the object into a JSON string.
-    
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
@@ -37,23 +33,36 @@ function App() {
 
     //Don't submit if the input is an empty string
     if (input !== "") {
-      setTodos([...todos, input]);
+      setTodos([...todos, { id: nanoid(), input: input }]);
     }
     setInput("");
   };
+
+  function deleteClickHandler(id) {
+    
+    const removeItem = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    setTodos(removeItem);
+  }
 
   return (
     <>
       <input
         type="text"
         value={input}
+        placeholder="Create new Todo"
         onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={clickHandler}>Add</button>
       <div>
         <ul>
           {todos.map((todo) => (
-            <li>{todo}</li>
+            <li key={todo.id}>
+              {todo.input}
+              <button onClick={() => deleteClickHandler(todo.id)}>Del</button>
+            </li>
           ))}
         </ul>
       </div>
